9a49d8c78cba949e7a7bf67eac2a7163e636a7ec
perfectyang <2406790623@qq.com>
2021-04-25 18:36:30 +0800

    调试

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk';
-// import { devToolsEnhancer } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import {Home} from './reducer/home'
 import {About} from './reducer/about'
 import {Login} from './reducer/login'
@@ -10,6 +10,6 @@ const reducers = combineReducers({
   Login
 })
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
